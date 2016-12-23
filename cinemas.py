1947ee0d1cf1bfd1322683359f2ea8cc46f40741
arseni-baranov <arseni-baranov@yandex.ru>
2016-12-23 21:49:22 +0500

    Внесёт большое количество полезных изменений
    Вынесет константы из класса
    Добавит необязательный консольный аргумент "--quantity", которым можно указать количество фильмов. Если аргумент не указан, скрипт парсит 10 фильмов
    Добавит проверку на наличие рейтинга в фильме на кинопоиске, как оказалось бывает что кинопоиск не знает о том что фильм уже в прокате, в таком случае возвращает ? для рейтинга и количества голосов
    Упростит цикл в fetch_movies, убирает от туда увеличенный timeout для случаев когда появляется exception. Теперь timeout статический прописан на 10 секунд.
    Заменит динамическое добавление в список рейтинга и количества голосов на генератор в функции fetch_movies
    Переименует функцию output_movies в print_movies, для большей понятности
    Уберёт повторяющийся код условия в output_movies
    Перепишет main(), теперь количество фильмов на вывод берётся из консольного аргумента quantity, список прокси-серверов формируется заранее и передаётся в генератор fetch_movies, оценки и количество проголосовавших берётся цикла с помощью генератора fetch_movies и добавляется в массив, теперь наличие параметра аргумента на сортированный вывод (args.showings) определяет наличие сортировки в print_movies.
    Уберёт все docstrings и комментарий, так как код теперь самдокументирован

--- cinemas.py
+++ cinemas.py
@@ -1,10 +1,3 @@
-""" Получает топ 10 самых рейтинговых фильмов которые на сегодянший день идут в кино
-    Использует прокси для обхода бана со стороны кинопоиска
-    
-    Стандартная сортировка идёт по общему рейнигу, возможна сортировка по количествам кинотеатров
-    в которых идёт показ
-"""
-
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -12,37 +5,36 @@ import random
 import argparse
 
 
-class Constants:
-    afisha_url = "http://www.afisha.ru/msk/schedule_cinema/"
-    kinopoisk_url = "http://kinopoisk.ru/index.php"
+afisha_url = "http://www.afisha.ru/msk/schedule_cinema/"
+kinopoisk_url = "http://kinopoisk.ru/index.php"
 
-    proxy_list = "http://www.freeproxy-list.ru/api/proxy?anonymity=false&token=demo"
-    proxy_re = re.compile(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{1,4}')
+proxy_list = "http://www.freeproxy-list.ru/api/proxy?anonymity=false&token=demo"
+proxy_re = re.compile(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{1,4}')
 
-    proxies = []
-
-    user_agents = [
-        'Mozilla/5.0 (X11; Linux i686; rv:50.0) Gecko/20100101 Firefox/50.0',
-        'Opera/9.80 (Windows NT 6.2; WOW64) Presto/2.12.388 Version/12.17',
-        'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0'
-    ]
+user_agents = [
+    'Mozilla/5.0 (X11; Linux i686; rv:50.0) Gecko/20100101 Firefox/50.0',
+    'Opera/9.80 (Windows NT 6.2; WOW64) Presto/2.12.388 Version/12.17',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0'
+]
 
 
 def get_console_args():
     parser = argparse.ArgumentParser(description="Shows top 10 movies sorted by rating currently in cinemas")
     parser.add_argument("--showings", "--s", help="Sort movies by cinema showings", action='store_true')
+    parser.add_argument("--quantity", "--q", help="Number of movies to print", default=10)
     args = parser.parse_args()
 
     return args
 
 
 def get_proxies_list():
-    html = requests.get(Constants.proxy_list).text
-    Constants.proxies = html.split('\n')
+    html = requests.get(proxy_list).text
+    proxies = html.split('\n')
+    return proxies
 
 
 def parse_afisha_page(max=10):
-    soup = BeautifulSoup(requests.get(Constants.afisha_url).text, "html.parser")
+    soup = BeautifulSoup(requests.get(afisha_url).text, "html.parser")
 
     schedule = soup.find_all("div", id="schedule")[0]
     divs = schedule.find_all("h3", class_="usetags", limit=max)
@@ -52,77 +44,61 @@ def parse_afisha_page(max=10):
            [len(theaters.findAll("td", class_="b-td-item")) for theaters in theater_tbl]
 
 
-def proxy_fetch_movies(movies, proxies_func=lambda: get_proxies_list(), debug_mode=True):
-
-    """ Получает список случайных прокси (для избежания бана со стороны кинопоиска),
-        парсит рейтинг и количество проголосовавших пользователей используя при этом
-        случайный прокси из полученного списка
-
-    :param movies: список названий фильмов
-    :param proxies_func: функция которая собирает случайные прокси
-                                    (в виде: 123.123.123.123:0000 \n)
-                                    
-    :param debug_mode: Вывод сообщений об ошибках
-
-    :return: Список с общими рейтингами и количеством проголосовавших пользователей
-    """
-
-    ratings = []
+def fetch_movies(movies, proxies):
     session = requests.Session()
 
-    proxies_func()
-
     for title in movies:
-        await = 20
         while 1:
             try:
                 print("Fetching movie:", title)
                 payload = {"kp_query": title, "first": "yes"}
-                header = {'User-Agent': random.choice(Constants.user_agents)}
-                proxies = {"http": random.choice(Constants.proxies)}
-
-                movie_html = session.get(Constants.kinopoisk_url, params=payload, headers=header,
-                                         timeout=await, proxies=proxies).text
+                header = {'User-Agent': random.choice(user_agents)}
+                proxy = {"http": random.choice(proxies)}
+                movie_html = session.get(kinopoisk_url, params=payload, timeout=10, headers=header, proxies=proxy).text
 
                 soup = BeautifulSoup(movie_html, "html.parser")
-                rating = soup.find("span", class_="rating_ball").text
-                people_rated = soup.find("span", class_="ratingCount").text.replace(u'\xa0', u' ')
-            except BaseException as error:
-                if debug_mode:
-                    print('\nError:', error)
-                    print('trying again...\n')
-                await += 10
+                movie_in_production = soup.findAll('td', text=re.compile('статус производства:'))
+                if movie_in_production:
+                    rating, people_rated = '?', '?'
+                else:
+                    rating = soup.find("span", class_="rating_ball").text
+                    people_rated = soup.find("span", class_="ratingCount").text.replace(u'\xa0', u' ')
+            except (AttributeError, requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError,
+                    requests.exceptions.ProxyError, requests.exceptions.ReadTimeout) as error:
+                print('\nError:', error)
+                print('trying again...\n')
             else:
-                ratings.append([rating, people_rated])
                 break
-    return ratings
+
+        yield [rating, people_rated]
 
 
-def output_movies(movie_data, cinema_sort=False):
-
-    # Отсортируем фильмы по рейтингу
-
+def print_movies(movie_data, cinema_sort=False):
     if cinema_sort:
-        movie_data.sort(key=lambda el: el[1], reverse=True)
+        sorting_function = lambda el: el[1]
     else:
-        movie_data.sort(key=lambda el: el[2][0], reverse=True)
+        sorting_function = lambda el: el[2][0]
+
+    movie_data.sort(key=sorting_function, reverse=True)
 
     for item in movie_data:
-        print("{0}:\nПоказ в {1} кинотеатрах\nНабрал: {2} по оценкам {3} пользователей\n"
+        print("\n{0}:\nПоказ в {1} кинотеатрах\nНабрал: {2} по оценкам {3} пользователей"
               .format(item[0], item[1], item[2][0], item[2][1]), sep="\n")
 
 
 def main():
     args = get_console_args()
-    movies, theaters = parse_afisha_page()
-    scores = proxy_fetch_movies(movies)
+    number_of_movies = int(args.quantity)
+    movies, theaters = parse_afisha_page(number_of_movies)
+    proxies = get_proxies_list()
+    scores = []
+
+    for item in fetch_movies(movies, proxies):
+        scores.append(item)
+
     movie_data = list(zip(movies, theaters, scores))
-
-    if args.showings:
-        output_movies(movie_data, cinema_sort=True)
-    else:
-        output_movies(movie_data, cinema_sort=False)
-
+    sorting = bool(args.showings)
+    print_movies(movie_data, cinema_sort=sorting)
 
 if __name__ == "__main__":
     main()
